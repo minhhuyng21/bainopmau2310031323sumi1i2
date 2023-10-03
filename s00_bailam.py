46b74b003bc627b2332dcbdb352bfaa843fedaed
minhhuyng21 <142973912+minhhuyng21@users.noreply.github.com>
2023-10-03 07:15:17 +0000

    Initial commit

---
+++ s00_bailam.py
@@ -0,0 +1,14 @@
+"""
+debai 
+Viet python ham sum()
+Nhap vao hai so nguyen i1, i2. Tinh tong hai so nay
+
+input 
+i1=1
+i2=22
+
+output 
+23
+"""
+def sum(i1,i2):
+  return i1 + i2
---
+++ s01_dapan.py
@@ -0,0 +1,5 @@
+def sum(i1,i2):
+  i1 = 1
+  i2 = 22
+  return i1 + i2
+'''Nam will add later'''
